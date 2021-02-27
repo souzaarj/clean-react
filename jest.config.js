430b82f7e4c458dac7079578082ee8af94e794c5
Nilson Souza <souza_arj@hotmail.com>
2021-02-27 13:37:25 -0300

    chore: config webpack

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.{ts|tsx}$': 'ts-jest'
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
