fa9db209d4e34ad14d1db7eb4823d0f9f97a013a
Nilson Souza <souza_arj@hotmail.com>
2021-03-08 20:33:03 -0300

    test: ensure Login calls validation with correct password

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -45,12 +45,21 @@ describe('Name of the group', () => {
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
-  test('should call validation with correct values', () => {
+  test('should call validation with correct email', () => {
     const { sut, validationSpy } = makeSut()
     const emailInput = sut.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: 'any_email' } })
     expect(validationSpy.input).toEqual({
       email: 'any_email'
+    })
+  })
+
+  test('should call validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
     })
   })
 })
