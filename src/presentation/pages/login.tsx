c530bc66228d075c0f49a623a83de020ded3eddc
Nilson Souza <souza_arj@hotmail.com>
2021-03-11 09:14:29 -0300

    feat: ensure Login call Authentication with correct values

--- src/presentation/pages/login.tsx
+++ src/presentation/pages/login.tsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react'
 import Styles from './login-styles.scss'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/context/form/form-context'
-import { Validation } from '../protocols'
+import { Validation } from '@/presentation/protocols'
+import { Authentication } from '@/domain/usecases/authentication'
 
 type Props = {
   validation: Validation
+  authentication: Authentication
 }
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -19,7 +21,6 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   })
 
   useEffect(() => {
-    console.log(state)
     setState(
       {
         ...state,
@@ -30,9 +31,17 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     )
   }, [state.email, state.password])
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+
+    console.log(state)
+
     setState({ ...state, isLoading: true })
+    await authentication.auth(
+      {
+        email: state.email,
+        password: state.password
+      })
   }
 
   return (
