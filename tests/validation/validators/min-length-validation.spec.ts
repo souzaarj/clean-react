3d7fa04a57f3d256fa52fed38dea4b78a65fbf36
Nilson Souza <souza_arj@hotmail.com>
2021-03-15 10:56:51 -0300

    test: ensure MinLengthValidation returns null if value is valid

--- tests/validation/validators/min-length-validation.spec.ts
+++ tests/validation/validators/min-length-validation.spec.ts
@@ -1,10 +1,17 @@
 import { MinLengthValidation } from '@/validation/validators'
 import { MinLengthError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('MinLengthValidation', () => {
   test('should return error if values is invalid', () => {
-    const sut = new MinLengthValidation('field', 5)
+    const sut = new MinLengthValidation(faker.random.word(), 5)
     const error = sut.validate('123')
     expect(error).toEqual(new MinLengthError())
+  })
+
+  test('should return null if value is valid', () => {
+    const sut = new MinLengthValidation(faker.random.word(), 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
   })
 })
