d59de4666c8d8f27155b725f769dcfd507558d14
Nilson Souza <souza_arj@hotmail.com>
2021-03-16 20:50:45 -0300

    test: ensure EmailValidation return null if email is valid

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -1,11 +1,17 @@
 import { InvalidEmailError } from '@/validation/errors/InvalidEmailError'
-import faker from 'faker'
 import { EmailValidation } from '@/validation/validators'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('should return error if e-mail is invalid', () => {
     const sut = new EmailValidation(faker.random.word())
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidEmailError())
+  })
+
+  test('should return null if e-mail is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
