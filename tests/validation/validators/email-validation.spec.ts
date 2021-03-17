501e71dc22608198d99e71ea517e610a867bd529
Nilson Souza <souza_arj@hotmail.com>
2021-03-16 21:00:13 -0300

    fix: ensure EmailValidation allows empty value

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -16,4 +16,10 @@ describe('EmailValidation', () => {
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
+
+  test('should return null if e-mail is empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
+    expect(error).toBeFalsy()
+  })
 })
