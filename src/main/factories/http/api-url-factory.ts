f71b78f1d486f9ed4b1656203e5155c8874405e5
Nilson Souza <souza_arj@hotmail.com>
2021-03-18 11:37:40 -0300

    feat: add env var to API URL

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,4 +1,4 @@
 
 export const makeApiUrl = (path: string): string => {
-  return `http://localhost:5050/api/${path}`
+  return `process.env.API_URL${path}`
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
   mode: 'development',
@@ -50,6 +51,9 @@ module.exports = {
     'react-dom': 'ReactDOM'
   },
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new DefinePlugin({
+      'process.env.API_URL': 'http://localhost:5050/api/'
+    })
   ]
 }
